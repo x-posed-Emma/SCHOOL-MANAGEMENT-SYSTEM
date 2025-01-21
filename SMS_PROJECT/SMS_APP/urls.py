d7268e9deb83bbb9ac0da5f549210a2bbac8306a
EmmanuelChisom <emereemmanuel9@gmail.com>
2025-01-21 13:37:21 +0100

    basics

--- SMS_PROJECT/SMS_APP/urls.py
+++ SMS_PROJECT/SMS_APP/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-# from .views import 
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     
-]
+]+ static(settings.STATIC_URL)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- SMS_PROJECT/SMS_PROJECT/settings.py
+++ SMS_PROJECT/SMS_PROJECT/settings.py
@@ -123,3 +123,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
