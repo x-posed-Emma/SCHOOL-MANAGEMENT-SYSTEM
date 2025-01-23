3d4028276349fd7961538b9dd17daf59da372544
Dera <dimemmanuel2889@gmail>
2025-01-23 06:19:12 -0800

    commiting anouncement

--- SMS_PROJECT/SMS_APP/views.py
+++ SMS_PROJECT/SMS_APP/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_list_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
-from .models import News
+from .models import News, Annoncement
 
 # @method_decorator(login_required, name='dispatch')
 class NewsListView(ListView):
@@ -13,4 +13,12 @@ class NewsListView(ListView):
     def get_queryset(self):
         # Filter news to ensure each has an author and a date
         return News.objects.filter(Date__isnull=False).order_by('-Date')
-# Author__isnull=False,
+    
+    
+class AnnouncementListView(ListView):
+    model = Annoncement
+    template_name = 'sms_app/announcement.html'  # Define your template name here
+    context_object_name = 'announcement_list'
+
+    def get_queryset(self):
+        return Annoncement.objects.filter(Date__isnull=False).order_by('-Date')
