55f7f75671379bbc8c55822bf33041c54c5862ab
EmmanuelChisom <emereemmanuel9@gmail.com>
2025-01-23 15:39:23 +0100

    over commit

---
+++ SMS_PROJECT/SMS_APP/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_list_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
+from .models import News
+
+# @method_decorator(login_required, name='dispatch')
+class NewsListView(ListView):
+    model = News
+    template_name = 'sms_app/news.html'  # Define your template name here
+    context_object_name = 'news_list'
+
+    def get_queryset(self):
+        # Filter news to ensure each has an author and a date
+        return News.objects.filter(Date__isnull=False).order_by('-Date')
